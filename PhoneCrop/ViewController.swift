50664e80e33856f2dbe29f8213efdb671ba8e8f6
Geison Goes <ggoes@thoughtworks.com>
2016-12-18 20:16:46 -0200

    Add images to scan and method to ocs the image

--- PhoneCrop/ViewController.swift
+++ PhoneCrop/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import TesseractOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
+
+    @IBOutlet weak var tvImageText: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let ocr = G8Tesseract(language: "eng") {
+            ocr.delegate = self
+            
+            //TODO: Get image from a photo taken
+            ocr.image = UIImage(named: "placa1")?.g8_blackAndWhite()
+            ocr.recognize()
+            
+            //TODO: Change it to get just the phone number from text
+            tvImageText.text = ocr.recognizedText
+            
+            //TODO: Create method to save number in agenda
+        }
+    }
+    
+    func progressImageRecognition(for tesseract: G8Tesseract!) {
+        print("Recognition progress: \(tesseract.progress)%")
     }
 
     override func didReceiveMemoryWarning() {
